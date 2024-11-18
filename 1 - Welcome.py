b6e18cf0479284238b63bad21612290e143d3754
mafer-hub <m.ferrari2303@gmail.com>
2024-11-18 23:16:27 +0100

    Add files via upload

---
+++ 1 - Welcome.py
@@ -0,0 +1,27 @@
+# hello! this is an attempt at learning how to use python and runa  code to evaluate the sentiment
+#everything that is written with a # is a comment, so python ignores them, but i-I can write here to explain
+
+#now let's start!
+#you can download python for free from its website
+#i use sublime text to write code - there are many alternatives, so please choose whatever you prefer! sublime text is also free
+#first thing to do: save the file with the .py extension (so something like name.py)
+#to make it work you need to "run it", as in to say "go ahead and do what i told you to do": to do this after writing the code you need to press ctrl + b
+#for example, if you write:
+
+print ('hello world')
+
+#print in python means "make this appear on the screen"
+#if you then press ctrl + b, in the lower part of the page the sentence "hello world" should appear
+#we can try again with something else, like
+
+print ('thank you for reading this!')
+
+#now this second sentence should appear too!
+#now something a bit more complicated
+
+first_name = 'name'
+last_name = 'surname'
+full_name = first_name + ' ' + last_name
+print(full_name)
+
+#we told python to print the full name, so it did (the ' ' is the space between the name and surname)
